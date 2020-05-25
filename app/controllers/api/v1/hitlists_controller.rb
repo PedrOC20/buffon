9ac1896808aed828c5e9ff780cb730a9ab764b8a
Pedro Candeias <pedromiguelcandeias@gmail.com>
2020-05-25 19:46:10 +0100

    First Commit

--- app/controllers/api/v1/hitlists_controller.rb
+++ app/controllers/api/v1/hitlists_controller.rb
@@ -1,6 +1,7 @@
 class Api::V1::HitlistsController < ApplicationController
   def index 
     @players = Player.where("player_type LIKE ?", "%Hitlist%")
+    
     # render :index, status: :ok
     render json: { status: 'SUCCESS', message: 'Hitlist Players', data: @players }, status: :ok
   end
