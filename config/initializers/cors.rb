24dfe7303e2e5b3e6370502f32c88b14b3c17b56
Pedro Candeias <pedromiguelcandeias@gmail.com>
2021-03-26 18:12:46 +0000

    Add route to cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,15 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'http://localhost:3001'
+
+    resource '*',
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
+
+  allow do
+    origins 'http://managment.mcvsports.pt'
 
     resource '*',
       headers: :any,
